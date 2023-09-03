49b7e369db05a4ea2d23223cb10bc3788a0e989e
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2023-09-03 20:39:30 +0300

    hide dot env

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,8 @@
+import { useSesion,  } from "next-auth"
+const page = () => {
+    return (
+        <div className='md:w-1/2 w-4/5 mx-auto text-white'>google auth</div>
+    )
+}
+
+export default page
--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='bg-[#0F0F0F] text-white'>
             <div className="md:w-1/2 w-4/5 mx-auto py-10 flex justify-between">
                 <Link href="/" className='hover:underline'>Buzz</Link>
-                <Link href="/signin" className='hover:underline'>Sign In</Link>
+                <Link href="/login" className='hover:underline'>Sign In</Link>
             </div>
         </div>
     )
